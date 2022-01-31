6ca7d8ea56ed029cc7db863afc6825a9d90daa0d
mesuk2043 <85116697+mesuk2043@users.noreply.github.com>
2022-01-31 09:36:57 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -18,7 +18,7 @@ class script(object):
 - <a href=https://t.me/MesukTarikul1000>Mesuk AK</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and BillieEilish will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
 1. Billie Eilish should have admin privillage.
@@ -40,7 +40,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/BillieEilish_Autobot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
