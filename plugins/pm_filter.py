93c9d28529312427be6778581c233c416955f068
mesuk2043 <85116697+mesuk2043@users.noreply.github.com>
2022-01-31 09:22:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -407,7 +407,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('� Updates', url='https://t.me/MesukTarikul1000')
+            InlineKeyboardButton(' Updates', url='https://t.me/MesukTarikul1000')
         ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -438,7 +438,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('� Updates', url='https://t.me/MesukTarikul1000'),
+            InlineKeyboardButton(' Updates', url='https://t.me/MesukTarikul1000'),
             InlineKeyboardButton('♥️ Source', callback_data='source')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
