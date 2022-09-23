fded7f201d408f8032c7a9c4d4ebe77cc30ae1ca
mesuk2043 <85116697+mesuk2043@users.noreply.github.com>
2022-09-23 17:12:27 +0600

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -385,7 +385,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         f_caption = files.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption = Join  @MesukTarikul1000 for more Movies.format(file_name='' if title is None else title,
+                f_caption = CUSTOM_FILE_CAPTION(file_name='' if title is None else title,
                                                        file_size='' if size is None else size,
                                                        file_caption='' if f_caption is None else f_caption)
             except Exception as e:
